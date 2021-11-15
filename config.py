4108a359821b268fa4431038ae4600152f213216
nic0mp <nico.mp215@gmail.com>
2021-11-14 21:52:09 -0600

    .env and forms.py added

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
-
+from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Give access to the project in ANY OS we find ourselves in
 # Allow outside files/folders to be added to the project from the
 # base directory
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config():
     """
@@ -12,6 +13,9 @@ class Config():
         Using Environment variables where available otherwise
         create the config variable if not done already.
     """
+
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # Turn off update messages from the sqlalchemy
